c6281215c047729384327158cd18c51b5c3f7ac4
kali-ch-1231 <kali8522009681@gmail.com>
2025-03-07 20:32:48 +0530

    Updated project

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom' 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter><StrictMode>
-  <App />
-</StrictMode></BrowserRouter>
-  
-)
+createRoot(document.getElementById("root")).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+);
