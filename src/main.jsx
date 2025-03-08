0c64406ea98072ab45d9f1783aa27fcf78f54508
kali-ch-1231 <kali8522009681@gmail.com>
2025-03-08 11:13:34 +0530

    sixth commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,6 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/MyPortfolio">
-  
-    <App />
-
+    <React.StrictMode></React.StrictMode>
   </BrowserRouter>
-  
 );
